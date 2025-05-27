4aaa67b00cc168018684c653f02aa21b4b8c31f1
BRUNA BOAVENTURA <f49808c@fiatauto.adfa.local>
2025-05-27 13:41:50 -0300

    Atualiza index.php Atualizando o título Tekim Farma

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 <html lang="pt-br">
 <head>
   <meta charset="UTF-8">
-  <title>Tekinho Farma</title>
+  <title>Tekim Farma</title>
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
 
   <header>
-    <h1>Tekinho Farma</h1>
+    <h1>Tekim Farma</h1>
   </header>
 
   <main class="content">
