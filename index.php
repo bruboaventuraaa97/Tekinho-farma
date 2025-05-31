3671e801a362905400eee2639666adce7c28f028
BRUNA BOAVENTURA <f49808c@fiatauto.adfa.local>
2025-05-30 21:37:38 -0300

    Ajuste tamanho logo

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
   <!-- TOPO -->
   <header class="bg-primary py-2">
   <div class="container d-flex align-items-center justify-content-center">
-    <img src="logo.png" alt="Logo Instituto" class="logo me-3">
+    <img src="logo-instituto-ana.png" alt="Logo Instituto" class="logo me-3">
     <h1 class="text-white mb-0 fs-2">Farma 💊</h1>
   </div>
 </header>
